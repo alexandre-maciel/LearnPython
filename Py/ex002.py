8f37872a46a86f332eccabef791bbd72b741153d
Alexandre Maciel <60267287+alexandre-maciel@users.noreply.github.com>
2025-03-25 17:06:58 -0300

    First Exercises

---
+++ Py/ex002.py
@@ -0,0 +1,12 @@
+n1 = int(input("me de um valor: "))
+n2 = int(input("me de outro valor: "))
+
+s= n1+n2
+m= n1*n2
+d= n1/n2
+di= n1//n2
+
+print(f"A soma de {n1} mais {n2} é igual a {s}")
+print(f"A multiplicação de {n1} vezes {n2} é igual a {m}")
+print(f"A divisão de {n1} dividido por {n2} é igual a {d}")
+print(f"A divisão inteira de {n1} dividido por {n2} é igual a {d}")
---
+++ Py/ex003.py
@@ -0,0 +1,2 @@
+# faça um programa que leia um numero inteiro e mostra na tela o seu sucessor e seu antecessor.
+
