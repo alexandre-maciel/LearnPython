488de224e93aec5d48debcb90e74430e786e005a
Alexandre Maciel <60267287+alexandre-maciel@users.noreply.github.com>
2025-10-25 14:43:04 +0000

    exercicios python curso

---
+++ Python Udemy/aula2.py
@@ -0,0 +1,26 @@
+'''
+tipos de dados
+
+Python = Linguagem de programação
+Tipo de tipagem = dinâmica / Forte
+str = string = texto
+
+string são textos que estão dentro de aspas
+
+'''
+
+print(1234)
+
+# aspas simples
+print('Luiz Otávio')
+
+# aspas duplas
+print("Luiz Otávio")
+
+# escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
+
+print('Luiz "Otávio"')
---
+++ Python Udemy/aula3.py
@@ -0,0 +1,31 @@
+'''
+tipos int e float
+int -> numero inteiro
+o tipo int representa qualquer numero positivo ou negativo
+int sem sinal é considerado positivo.
+'''
+
+print(11)
+print(-11)
+print(0)
+print(1.2)
+
+'''
+
+float -> numero com ponto flutuante
+o tipo float representa qualquer numero positivo ou negativo com ponto flutuante
+float sem sinal é considerado positivo
+
+a função type mostra o tipo que o python inferiu ao valor
+
+callable -> chamavel
+
+class
+
+tudo em python é um objeto
+'''
+
+print(23.4)
+print(-5.2)
+
+print(type(5.2))
