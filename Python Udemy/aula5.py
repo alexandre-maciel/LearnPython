4b75cf88f07a7a509e0c6cb650e9e20a25362c92
Alexandre Maciel <60267287+alexandre-maciel@users.noreply.github.com>
2025-10-26 21:31:02 +0000

    dia 26-10

---
+++ Python Udemy/aula5.py
@@ -0,0 +1,11 @@
+'''
+coerção de tipos, coerção 
+type conversion, typecasting, coercion
+é o ato de converter um tipo em outro
+tipos imutáveis e primitivos: str, int, float, bool
+
+linguagens dinamicas podem fazer a soma de duas str
+'''
+
+print (1+1)
+print ('a' + 'b')
